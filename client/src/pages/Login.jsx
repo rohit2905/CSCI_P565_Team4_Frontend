c209f770026a5dce362de474cbd07071e0c09422
rohit2905 <krohitgoud@gmail.com>
2024-03-02 19:27:40 -0500

    CAPTCHA

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,16 +7,18 @@ import Select from '@mui/material/Select';
 
 // design
 import {
-	Button,
+  Button,
   FormControl,
   IconButton,
-  Input,
   InputAdornment,
   InputLabel,
   TextField,
   Typography,
+  CardContent,
+  CardActions,
   Container,
-  Box, Grid, CssBaseline, Paper, FormControlLabel, Checkbox, OutlinedInput
+  FormHelperText,
+  Box, Grid, CssBaseline, Paper, OutlinedInput
 } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
@@ -24,70 +26,92 @@ import MenuItem from '@mui/material/MenuItem';
 import Link from '@mui/material/Link';
 import LoginImg from '../Assets/login.png';
 
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 // functions
 import { login } from "../api/user";
-import Header from "../components/Header";
 
 
 const Login = () => {
-	const history = useNavigate();
-	const { user, setUser } = useContext(UserContext);
-	const { usertype, setUsertype } = useContext(UserContext);
-	const { useremail, setUseremail } = useContext(UserContext);
-	const { userhome, setuserhome } = useContext(UserContext);
-	//var check = undefined;
+  const history = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const { usertype, setUsertype } = useContext(UserContext);
+  const { useremail, setUseremail } = useContext(UserContext);
+  const { userhome, setuserhome } = useContext(UserContext);
 
-	// form states
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [showPassword, setShowPassword] = useState(false);
-	const [userType, setUserType] = useState("");
-	const [otp, setOTP] = useState("");
-	const [otpSent, setotpSent] = useState(false);
+  // form states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [userType, setUserType] = useState("");
+  const [otp, setOTP] = useState("");
+  const [otpSent, setotpSent] = useState(false);
+  const [passReset, setPassReset] = useState(false);
+  const randomString = Math.random().toString(36).slice(8);
+  const [captcha, setCaptcha] = useState(randomString);
+  const [text, setText] = useState("");
+  const [valid, setValid] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [captchaReq, setCaptchaReq] = useState(false);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if(captchaReq){
+      alert("CAPTCHA Not Verified!");
+      return;
+    }
+    try {
+      const res = await login({userType, email, password, otp });
+      if(res.passReset){
+        setCaptchaReq(true);
+        setPassReset(res.passReset);
+      }
+      if (res.error) toast.error(res.error);
+      else {
+        toast.success(res.message);
+        setotpSent(1);
+        setUser(res.username);
+        setUsertype(res.userType);
+        setUseremail(res.email);
+        if(res.userType === "10"){
+          setuserhome("/Customer");
+        } else if(res.userType === "20"){
+          setuserhome("/Driver");
+        } else if(res.userType === "30"){
+          setuserhome("/Manager");
+        }
+        
+      }
+    } catch (err) {
+      toast.error(err);
+    }
+    console.log(passReset);
+  };
 
-	if ((userType) === 10){
-		var url = '../Customer'
-	};
-	if ((userType) === 20){
-		var url = '../Driver'
-	};
-	if ((userType) === 30){
-		var url = '../Manager'
-	};	
-	const handleLogin = async (e) => {
-		//var check =1;
-		e.preventDefault();
-		//console.log("Disable is set");
-		//console.log(check);
-		
-		try {
-			const res = await login({userType, email, password, otp });
-			if (res.error) toast.error(res.error);
-			else {
-				toast.success(res.message);
-				setotpSent(1);
-				setUser(res.username);
-				setUsertype(res.userType);
-				setUseremail(res.email);
-				if(res.userType == "10"){
-					setuserhome("/Customer");
-				} else if(res.userType == "20"){
-					setuserhome("/Driver");
-				} else if(res.userType == "30"){
-					setuserhome("/Manager");
-				}
-								
-			}
-		} catch (err) {
-			toast.error(err);
-		}
-	};
+  let navigate = useNavigate();
 
-	let navigate = useNavigate();
-	return !user ? (
-		
-		<Container component="main" maxWidth="lg">
+  const refreshString = () => {
+    setCaptchaReq(true);
+    setText("");
+    setCaptcha(Math.random().toString(36).slice(8));
+  };
+
+  const matchCaptcha = (event) => {
+    event.preventDefault();
+    setButtonClicked(true);
+    if (text === captcha) {
+      setValid(false);
+      setSuccess(true);
+      setCaptchaReq(false);
+    } else {
+      setValid(true);
+      setSuccess(false);
+      setCaptchaReq(true);
+    }
+  };
+  return !user ? (
+    <Container component="main" maxWidth="lg">
+      
       <Box
         sx={{
           marginTop: 8,
@@ -102,8 +126,8 @@ const Login = () => {
             md={7}
             sx={{
               backgroundImage: `url(${LoginImg})`,
-            backgroundRepeat:'no-repeat',
-        	backgroundSize: "contain"
+              backgroundRepeat:'no-repeat',
+              backgroundSize: "contain"
             }}
           />
           <Grid
@@ -132,21 +156,21 @@ const Login = () => {
                 noValidate
                 sx={{ mt: 1 }}
               >
-				<FormControl fullWidth>
-      <InputLabel id="demo-simple-select-helper-label">User Type</InputLabel>
-      <Select
-        labelId="demo-simple-select-helper-label"
-        id="demo-simple-select-helper"
-        value={userType}
-		autoFocus
-        label="User Type"
-        onChange={(e) => setUserType(e.target.value)}
-      >
-        <MenuItem value={10}>Customer</MenuItem>
-        <MenuItem value={20}>Driver</MenuItem>
-        <MenuItem value={30}>Admin</MenuItem>
-      </Select>
-    </FormControl>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-helper-label">User Type</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={userType}
+                    autoFocus
+                    label="User Type"
+                    onChange={(e) => setUserType(e.target.value)}
+                  >
+                    <MenuItem value={10}>Customer</MenuItem>
+                    <MenuItem value={20}>Driver</MenuItem>
+                    <MenuItem value={30}>Admin</MenuItem>
+                  </Select>
+                </FormControl>
                 <TextField
                   margin="normal"
                   required
@@ -155,68 +179,122 @@ const Login = () => {
                   name="email"
                   autoComplete="email"
                   autoFocus
-				  size="small"
-				  variant="outlined"
-				  label="Email"
-				  value={email}
-				  onChange={(e) => setEmail(e.target.value)}
+                  size="small"
+                  variant="outlined"
+                  label="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-        <FormControl fullWidth variant="outlined" margin="normal" required>
-      <InputLabel htmlFor="password">Password</InputLabel>
-      <OutlinedInput
-        type={showPassword ? 'text' : 'password'}
-        value={password}
-		size="small" 
-		autoFocus
-		required
-        onChange={(e) => setPassword(e.target.value)}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={() =>
-					setShowPassword(!showPassword)
-				}
-              edge="end"
+                <FormControl fullWidth variant="outlined" margin="normal" required>
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <OutlinedInput
+                    type={showPassword ? 'text' : 'password'}
+                    value={password}
+                    size="small" 
+                    autoFocus
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={() =>
+                            setShowPassword(!showPassword)
+                          }
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+                <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                  <Button
+                    autoFocus
+                    variant="contained"
+                    disabled={!email || !password}
+                    onClick={handleLogin}
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Get OTP
+                  </Button>
+                  <TextField
+                    margin="normal"
+                    required
+                    name="otp"
+                    autoFocus
+                    size="small"
+                    variant="outlined"
+                    type="text" 
+                    label="OTP"
+                    disabled={!otpSent} 
+                    value={otp}
+                    onChange={(e) => setOTP(e.target.value)}
+                  />
+                </div>
+               
+                {passReset && (
+        <CardContent>
+          <CardActions>
+            <div style={{
+                      color: '#fff',
+                      textDecoration: 'line-through',
+                      userSelect: 'none',
+                      backgroundColor: '#4e8ab5',
+                      borderRadius: '3px',
+                      width: '100px', // Adjusted width for better visibility
+                      height: '30px', // Adjusted height for better visibility
+                      fontSize: '20px', // Adjusted font size for better visibility
+                      marginBottom: 'auto',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      fontFamily: 'Hanalei Fill, cursive', // Specify the font families
+
+                      fontStyle: 'italic', // Bold font weight for emphasis
+                    }}>{captcha}</div>
+            <Button
+              startIcon={<RefreshIcon />}
+              onClick={refreshString}
+              size="small"
             >
-              {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-            </IconButton>
-          </InputAdornment>
-        }
-        label="Password"
-      />
-    </FormControl>
-	<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-		<Button
-		autoFocus
-        variant="contained"
-		disabled={!email || !password}
-		onClick={handleLogin}
-		sx={{ mt: 3, mb: 2 }}
-      >
-        Get OTP
-      </Button>
-      <TextField
-			margin="normal"
-			required
-			name="otp"
-			autoFocus
-			size="small"
-			variant="outlined"
-			type="text" 
-			label="OTP"
-			disabled = {!otpSent} 
-			value={otp}
-			onChange={(e) => setOTP(e.target.value)}
-			/>
-      
-    </div>
+              Refresh
+            </Button>
+          </CardActions>
+            <TextField
+              disabled={!otp}
+              label="Enter Captcha"
+              required
+              autoFocus
+              size="small"
+              value={text}
+              name="captcha"
+              variant="outlined"
+              onChange={(e) => setText(e.target.value)}
+              error={valid}
+              helperText={
+              buttonClicked && (<FormHelperText 
+                        sx={{ color: valid ? 'red' : text.length  > 0 ? 'green' : 'inherit' }}
+                      >
+                        {valid ? "Invalid Captcha" : text.length > 0 ? "Captcha Verified" : ""}
+                      </FormHelperText>)}
+            />
+            <Button
+              autoFocus
+              variant="contained"
+              disabled={!email || !password ||!otp}
+              sx={{ mb: 4, ml:1 }}
+              onClick={matchCaptcha}>Verify</Button>
+        </CardContent>
+      )}
                 <Button
-				autoFocus
-				  fullWidth
+                  autoFocus
+                  fullWidth
                   variant="contained"
-				  disabled={!otp}
-				  onClick={handleLogin}
+                  disabled={!otp}
+                  onClick={handleLogin}
                   sx={{ mt: 3, mb: 2 }}
                 >
                   Log In
@@ -240,8 +318,8 @@ const Login = () => {
       </Box>
     </Container>
   ) : (
-		<Navigate to={'../Customer'}/>
-	);
+    <Navigate to={'../Customer'}/>
+  );
 };
 
 export default Login;
